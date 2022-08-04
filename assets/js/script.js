f3cd71df6992a88cdd1eb7f33040c2c77c3cf93b
IvetteMcDermott <ivettedemcdermott@gmail.com>
2022-08-04 22:21:10 +0000

    add multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,13 +29,14 @@ function runGame(gameType){
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1,num2);
+    } else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1,num2);
     } else {
         alert (`Unknow game type: ${gameType}`);
         throw `Unknow game type ${gameType}. Aborting!`
-
     }
 
-}
+    }
 
 /**
  * checks the answer against the first element in
@@ -68,12 +69,16 @@ function calculateCorrectAnswer(){
     if (operator === "+") {
 
         return [operand1 + operand2, "addition"];
-    } else {
+
+    } else if (operator === "x") {
+
+            return [operand1 * operand2, "multiply"];
+    }   else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
     }
-
+    
 /**
  * gets the current score from the DOM and increment it by 1
  */
@@ -99,10 +104,12 @@ function displayAdditionQuestion(operand1, operand2){
 }
 
 function displaySubtractQuestion(){
-    
+
 }
 
-function displayMultiplyQuestion(){
-    
+function displayMultiplyQuestion(operand1,operand2){
+    document.getElementById("operand1").textContent= operand1;
+    document.getElementById("operand2").textContent= operand2;
+    document.getElementById("operator").textContent= "x";    
 }
 
