c91b53f0e81e8f07368495568fb32816837a24c5
IvetteMcDermott <ivettedemcdermott@gmail.com>
2022-08-04 16:58:14 +0000

    add code to increment scrore and incorrect answers tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,8 +48,10 @@ function checkAnswer(){
 
     if (isCorrect) {
         alert("Hey! You got it right! :D");
+        incrementScore();
     } else {
         alert(`Awwww... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer()
     }
     runGame(calculatedAnswer[1]);
 }
@@ -72,13 +74,21 @@ function calculateCorrectAnswer(){
     }
     }
 
+/**
+ * gets the current score from the DOM and increment it by 1
+ */
 
-function IncrementScore(){
-
+function incrementScore(){
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 }
 
-function IncrementWrongAnswer(){
-    
+/**
+ * gets the current tally of incorrect answers from the DOM and increments by 1
+ */
+function incrementWrongAnswer(){
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2){
